27436ae2516640b46dc2b5232db61333b8d87d9d
Jalen DeLoney <jalendeloney@gmail.com>
2025-07-17 13:19:30 -0700

    added delete comment

--- app/flask_app.py
+++ app/flask_app.py
@@ -237,6 +237,19 @@ def view_episode(episode_id):
     comments = Comment.query.filter_by(episode_id=int(episode_id)).order_by(Comment.timestamp).all()
     return render_template('episode_page.html', episode=episode, form=form, comments=comments)
 
+@app.route('/comment/<int:comment_id>/delete', methods=['POST'])
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.get_or_404(comment_id)
+
+    if comment.user_id != current_user.id:
+        flash("You don't have permission to delete this comment.", "danger")
+        return redirect(request.referrer or url_for('catalogue'))
+
+    db.session.delete(comment)
+    db.session.commit()
+    flash("Comment deleted.", "success")
+    return redirect(request.referrer or url_for('catalogue'))
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
