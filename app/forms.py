0020c62e4df64432054746abfd8a77a1b29f0e06
Andrew Lee <andrew1112.lee@gmail.com>
2025-07-24 19:29:18 +0000

    added login live feedback and regex

--- app/forms.py
+++ app/forms.py
@@ -12,21 +12,44 @@ from app.models import User
 
 
 class RegistrationForm(FlaskForm):
+    # username validataion fields
     username = StringField(
-        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+        "Username",
+        validators=[
+            DataRequired(),
+            Length(min=2, max=20),
+            Regexp(r"^[A-Za-z0-9_.]+$", message="Letters, numbers, ., and _ only."),
+        ],
     )
-    password = PasswordField("Password", validators=[DataRequired()])
+
+    # password validataion fields
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(min=8),
+            Regexp(
+                # must contain at least one upper, lower, digit, and special character
+                r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$",
+                message="Use letters, numbers, and special characters.",
+            ),
+        ],
+    )
+
+    # double check password validataion fields
     confirm_password = PasswordField(
-        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+        "Confirm Password",
+        validators=[DataRequired(), EqualTo("password", message="Passwords must match.")],
     )
+
+    # submit validataion fields(?)
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
         # Check if username already exists
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError(
-                "Username is already taken. Please choose another.")
+            raise ValidationError("Username is already taken. Please choose another.")
 
 
 class LoginForm(FlaskForm):
