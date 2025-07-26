a17ea00674329a915697b160089c9b9379a5a9a3
Jalen DeLoney <jalendeloney@gmail.com>
2025-07-26 02:58:00 -0400

    use higher qual tmdb images, tweaks to catalogue styling

--- app/tmdb.py
+++ app/tmdb.py
@@ -8,7 +8,7 @@ load_dotenv()
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 BASE_URL = "https://api.themoviedb.org/3"
-IMG_BASE_URL = "https://image.tmdb.org/t/p/w500"
+IMG_BASE_URL = "https://image.tmdb.org/t/p/w780"
 
 # Fetch multiple pages of popular movies or TV shows
 def fetch_popular(media_type="movie", pages=1):
